fc1dce45540da2d0c7be7fc5e2749df4c1799e96
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-27 16:35:57 -0600

    updated

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+public static void main(String[] args) {
+
+//    double pi = 3.14159;
+//    System.out.printf("The value of pi is approximately %.2f", pi);
+
+    Scanner scanner = new Scanner(System.in);
+//    System.out.print("Enter a number: ");
+//    int number = scanner.nextInt();
+//    System.out.println("you entered " + number);
+
+//    System.out.print("enter three words: ");
+//    String word1 = scanner.next();
+//    String word2 = scanner.next();
+//    String word3 = scanner.next();
+//    System.out.println(word1);
+//    System.out.println(word2);
+//    System.out.println(word3);
+
+    System.out.print("enter a sentence: ");
+    String sentence = scanner.nextLine();
+    System.out.println(sentence);
+
+
+
+
+
+
+
+
+
+    }
+
+}
