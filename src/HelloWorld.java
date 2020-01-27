5d9af976987598dfaf2440a558248717f740b3ea
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-27 10:59:07 -0600

    completed first exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,15 @@
 public class HelloWorld {
 public static void main(String[] args) {
     System.out.println("Hello World");
-}
+    int myFavoriteNumber = 5;
+    System.out.println(myFavoriteNumber);
+    String myString = "Matt";
+    System.out.println(myString);
+
+
+
+
+
+    }
 
 }
