1957e9cc9bd4c6327d5ee2a395c5e9f8356ed983
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-27 13:45:59 -0600

    updated

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,20 @@
 public class HelloWorld {
 public static void main(String[] args) {
-    System.out.println("Hello World");
-    int myFavoriteNumber = 5;
-    System.out.println(myFavoriteNumber);
-    String myString = "Matt";
-    System.out.println(myString);
+//    System.out.println("Hello World");
+//    int myFavoriteNumber = 5;
+//    System.out.println(myFavoriteNumber);
+//    String myString = "Matt";
+//    System.out.println(myString);
+//    float myNumber = 3.14f;
+//    System.out.println(myNumber);
 
+//    int x = 5;
+//    System.out.println(x++);
+//    System.out.println(x);
 
+//    int x = 5;
+//    System.out.println(++x);
+//    System.out.println(x);
 
 
 
