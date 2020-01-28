c833ce5c81d046c077a05881aed35f9f58752a0a
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-28 12:54:49 -0600

    updated

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -6,9 +6,20 @@ public static void main(String[] args) {
 //
 //}
 
-for(int i = 2; i <= 1_000_000; i++) {
-    System.out.println(i);
-}
+//int num = 0;
+//do {
+//    System.out.println(num);
+//    num+=2;
+//} while(num <= 100);
+
+double num = Math.pow(2, 2);
+do {
+    System.out.println(num);
+    num++;
+} while(num <= 1_000_000);
+
+
+
 
 
 
