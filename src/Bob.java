f9980417dc2faf0947313dd1b1d1d1dfa92a27a0
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-29 14:53:30 -0600

    updated

--- src/Bob.java
+++ src/Bob.java
@@ -2,18 +2,16 @@ import java.util.Scanner;
 
 public class Bob {
 public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Say something to Bob: ");
-    String something = scanner.next();
-    if(something.endsWith("?"))
-        System.out.println("sure");
-    else if(something.endsWith("!"))
-        System.out.println("Whoa, chill out!");
-    else
-        System.out.println("whatever");
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Say something to Bob: ");
+        String something = scanner.next();
+        if (something.endsWith("?"))
+           System.out.println("sure");
+       else if (something.endsWith("!"))
+            System.out.println("Whoa, chill out!");
+        else
+            System.out.println("whatever");
 
 
     }
-
 }
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,11 +1,27 @@
 public class MethodsExercises {
-public static void main(String[] args) {
+    public static void main(String[] args) {
+
 
 
 
     }
 
-    public static int Addition(int num, int num2) {
-            return num + num2;
+
+    public static int add(int num, int num2) {
+        return num + num2;
     }
+
+    public static int sub(int num, int num2) {
+        return num - num2;
+    }
+
+    public static int mult(int num, int num2) {
+        return num * num2;
+    }
+
+    public static int div(int num, int num2) {
+        return num / num2;
+    }
+
+
 }
