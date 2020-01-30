ac6c5d6d3f1b77c74237431ec9cf4c25274bd058
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-30 10:22:49 -0600

    started adventure game

---
+++ src/AdventureGame.java
@@ -0,0 +1,21 @@
+public class AdventureGame {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+    int smallEnemyHealth = 2;
+    int largeEnemyHealth = 4;
+    int yourHealth = 5;
+    int healthStim = 0;
+    int healthstimAmount = 3;
+    int stimPackAfterEnemy = 1;
+    public static void mainAdventureGame() {
+
+
+    }
+
+
+}
