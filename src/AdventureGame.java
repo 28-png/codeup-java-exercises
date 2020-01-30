8ba05c250ddae63854386d46a86afd69bbc195d5
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-30 10:48:21 -0600

    updated

--- src/AdventureGame.java
+++ src/AdventureGame.java
@@ -12,7 +12,17 @@ public class AdventureGame {
     int healthStim = 0;
     int healthstimAmount = 3;
     int stimPackAfterEnemy = 1;
+    int movement = (int) (Math.random() * 40 + 1);
+    int smallEnemyDamage = (int) (Math.random() * 50 + 1);
+    int largeEnemyDamage = (int) (Math.random() * 50 + 1);
+    int OurDamagelargeEnemy = (int) (Math.random() * 50 + 1);
+    int ourDamageSmallEnemy = (int) (Math.random() * 50 + 1);
     public static void mainAdventureGame() {
+
+
+   }
+
+   public static void forwardAdventureGame() {
 
 
    }
