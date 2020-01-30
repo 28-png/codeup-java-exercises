1d417f3e2c2d0c8a3db7d25bcb38a352e22f9e54
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-30 15:04:31 -0600

    updated

--- src/AdventureGame.java
+++ src/AdventureGame.java
@@ -1,48 +1,122 @@
+import java.util.Scanner;
+
 public class AdventureGame {
     public static void main(String[] args) {
 
-
-
-
+    mainAdventureGame();
 
     }
-    int smallEnemyHealth = 2;
-    int largeEnemyHealth = 4;
-    int yourHealth = 5;
-    int healthStim = 0;
-    int healthstimAmount = 3;
-    int stimPackAfterEnemy = 1;
-    int movement = (int) (Math.random() * 40 + 1);
-    int smallEnemyDamage = (int) (Math.random() * 50 + 1);
-    int largeEnemyDamage = (int) (Math.random() * 50 + 1);
-    int OurDamagelargeEnemy = (int) (Math.random() * 50 + 1);
-    int ourDamageSmallEnemy = (int) (Math.random() * 50 + 1);
-    public static void mainAdventureGame() {
+
+
+
+//    variables
+    static Scanner scanner = new Scanner(System.in);
+    static String moving, smallEnemy, largeEnemy;
+   static int smallEnemyHealth = 2;
+   static int largeEnemyHealth = 4;
+   static int yourHealth = 5;
+   static int healthStim = 0;
+   static int healthstimAmount = 3;
+   static int stimPackAfterEnemy = 1;
+   static int movement = (int) (Math.random() * 40 + 1);
+   static int ourDamageSmallEnemy, ourDamageLargeEnemy, largeEnemyDamage, smallEnemyDamage = (int) (Math.random() * 50 + 1);
+// end of main variables
+
+
+
+//  main method:
+
+
+   public static void mainAdventureGame() {
+        System.out.println("the controls are forward, left, right, back, hit to attack, stim for health, \n" +
+                "status for how much health and stim packs you have, and block to block");
+        System.out.print("What direction do you want to go? \n");
+       moving = scanner.next();
+       do {
+               forwardAdventureGame();
+               leftAdventureGame();
+               rightAdventureGame();
+               backAdventureGame();
+       }while(!moving.equals("run"));
+
 
 
    }
 
-   public static void forwardAdventureGame() {
+//  moving forward method:
 
+    static boolean forwardSmallEnemy = movement < 20 && movement > 10;
+    static boolean forwardLargeEnemy = movement < 10 && movement > 1;
 
+    public static void forwardAdventureGame() {
+       do {
+           if (forwardSmallEnemy)
+               System.out.println("you have encountered a small enemy! attack!\n");
+           else if (forwardLargeEnemy)
+               System.out.println("You have encountered a large enemy! attack!\n");
+           else
+               System.out.println("Lets keep going where do we go from here? \n");
+           moving = scanner.next();
+       } while(moving.equals("forward"));
    }
+
+
+
+//   moving left method:
+
+   static boolean leftSmallEnemy = movement < 40 && movement > 30;
+   static boolean leftLargeEnemy = movement < 30 && movement > 20;
 
    public static void leftAdventureGame() {
+       do {
+           if (leftSmallEnemy)
+               System.out.println("you have encountered a small enemy! attack!\n");
+           else if (leftLargeEnemy)
+               System.out.println("You have encountered a large enemy! attack!\n");
+           else
+               System.out.println("Lets keep going where do we go from here? \n");
+           moving = scanner.next();
+       }while(moving.equals("left"));
+   }
 
 
-    }
 
+//   moving right method:
+
+    static boolean rightSmallEnemy = movement < 20 && movement > 10;
+    static boolean rightLargeEnemy = movement < 40 && movement > 30;
 
     public static void rightAdventureGame() {
-
-
+        do {
+            if (rightSmallEnemy)
+                System.out.println("you have encountered a small enemy! attack!\n");
+            else if (rightLargeEnemy)
+                System.out.println("You have encountered a large enemy! attack!\n");
+            else
+                System.out.println("Lets keep going where do we go from here? \n");
+            moving = scanner.next();
+        }while(moving.equals("right"));
     }
 
+
+//        moving back method:
+
+    static boolean backSmallEnemy = movement < 10 && movement > 1;
+    static boolean backLargeEnemy = movement < 30 && movement > 20;
 
     public static void backAdventureGame() {
-
-
+        do {
+            if (backSmallEnemy)
+                System.out.println("you have encountered a small enemy! attack!\n");
+            else if (backLargeEnemy)
+                System.out.println("You have encountered a large enemy! attack!\n");
+            else
+                System.out.println("Lets keep going where do we go from here? \n");
+            moving = scanner.next();
+        }while(moving.equals("back"));
     }
+
+
 
 
 }
--- src/HighLow.java
+++ src/HighLow.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class HighLow {
 public static void main(String[] args) {
 
+            int x = 5 * 4 % 3;
+            System.out.println(x);
 
-gameHighLow();
-
+//gameHighLow();
 
     }
 
