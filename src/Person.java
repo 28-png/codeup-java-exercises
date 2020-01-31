40e06e6d98b13d7e01a0e0ee91fff4d3f5cac831
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-31 11:27:01 -0600

    updated

--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,30 @@
 public class Person {
+private String name;
+
+    public static void main(String[] args) {
 
 
 
+
+    }
+
+    // returns the person's name
+    public String getName() {
+        return this.name;
+    }
+
+
+    // changes the name property to the passed value
+    public void setName(String name) {
+
+
+    }
+
+
+    // prints a message to the console using the person's name
+    public void sayHello() {
+
+
+    }
 
 }
