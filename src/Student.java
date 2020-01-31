2b38361cd275da0a42ac2b28fd8f5f1bb0915012
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-31 10:05:46 -0600

    updated

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,4 @@
+import javax.naming.ldap.LdapName;
 import java.util.Date;
 
 public class Student {
@@ -11,15 +12,33 @@ public class Student {
     public static void main(String[] args) {
     Student matt = new Student();
         matt.name = "Matt";
+        matt.cohort = "Europa";
+        matt.startDate = new Date("06/11/2018");
+        matt.program = "Web Development";
+        matt.location = "San Antonio";
+        matt.drinksCoffee = true;
+        System.out.println(matt.report());
 
-        Student zach = new Student();
+       Student zach = new Student();
         zach.name = "Zach";
+        zach.cohort = "Bayes";
+        zach.startDate = new Date("February 11, 2017");
+        zach.program = "Data Science";
+        zach.location = "San Antonio";
         zach.drinksCoffee = false;
+        System.out.println(zach.report());
 
-        System.out.println(matt.name);
-        if(zach.drinksCoffee)
-        System.out.println(zach.name + " drinks coffee");
+    }
 
+    public String report() {
+        String output = "";
+        output += "My name is " + this.name + ". ";
+        output += "I started learning " + this.program + " with " + this.cohort + " on " + this.startDate + ". ";
+       if(this.drinksCoffee)
+           output += "I drink coffee in " + this.location + ". ";
+       else
+           output += "I dont drink coffee in " + this.location + ".";
+       return output;
     }
 
 
