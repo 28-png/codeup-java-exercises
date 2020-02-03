caad1026abca85aebc9aae8bbc34f8dab50f00f5
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-03 09:15:25 -0600

    completed morning warmup

--- src/ProgrammingPushups.java
+++ src/ProgrammingPushups.java
@@ -1,6 +1,7 @@
 public class ProgrammingPushups {
 public static void main(String[] args) {
     System.out.println(multiply(7, 8));
+    System.out.println(sumOfBinary("11", "10"));
 
     }
 
@@ -11,6 +12,13 @@ public static void main(String[] args) {
         }
         return total;
 
+    }
+
+    public static String sumOfBinary(String first, String second) {
+        int b1 = Integer.parseInt(first, 2);
+        int b2 = Integer.parseInt(second, 2);
+        int sum = b1 + b2;
+        return Integer.toBinaryString(sum);
     }
 
 
