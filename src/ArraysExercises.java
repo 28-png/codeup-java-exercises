7212e1938ac1e330bef980c4dc299d3acd188daa
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-03 13:41:48 -0600

    updated

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -2,28 +2,36 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class ArraysExercises {
+    String[] persons = {"Matt", "John", "Dillan"};
 
-    public static String addPerson() {
-        String[] newArr = {};
-        return addPerson();
+
+
+
+    public static void addPerson(String[] arrName, String name) {
+        String[] newArray = new String[arrName.length +1];
+        System.arraycopy(arrName, 0, newArray, 0, arrName.length);
+        newArray[newArray.length -1] = name;
+        System.out.println(Arrays.toString(newArray));
+        for (String s : newArray) {
+            System.out.println(s);
+        }
 
     }
-    public static void names() {
+
+
+
+
+    public static void main(String[] args) {
         String[] persons = {"Matt", "John", "Dillan"};
         Person person = new Person(Arrays.toString(persons));
         System.out.println(person.getName());
-        addPerson();
 //        int[] numbers = {1, 2, 3, 4, 5};
 //        System.out.println(Arrays.toString(numbers));
         for (String p : persons) {
             System.out.println(p);
         }
-    }
-    public static void main(String[] args) {
-       names();
 
-
-
+    addPerson(persons, "Fred");
 
 
     }
