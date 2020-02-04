17cc6ac10f724051ea575b56d68981a6701fe642
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-04 13:03:43 -0600

    updated

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,6 +1,17 @@
 package shapes;
 
-public class Square {
+public class Square extends Rectangle {
 
+     Square(int side) {
+        super(side, side);
+        }
+        public int getArea() {
+            return (int) Math.pow(this.length, 2);
+
+     }
+
+     public int getPerimeter() {
+         return 4 * this.length;
+     }
 
 }
