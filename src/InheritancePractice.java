cc7b0dc5e48e632d6d65b826b9dfbaee18535c57
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-04 08:15:21 -0600

    added inheritance practice

---
+++ src/InheritancePractice.java
@@ -0,0 +1,30 @@
+public class InheritancePractice {
+//    public String name;
+//
+//    public InheritancePractice(String name) {
+//        this.name = name;
+//    }
+//
+//    public void sayHello() {
+//        System.out.println("Hello from " + name + "!");
+//    }
+//
+//    static class Employees extends InheritancePractice {
+//
+//        public Employees(String name) {
+//            super(name);
+//        }
+//    }
+
+
+
+
+
+    public static void main(String[] args) {
+//        Employees john = new Employees("John");
+//        john.sayHello();
+
+}
+
+
+}
