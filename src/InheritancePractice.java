36a82636af7e0a10797cb80b1a5607d751eec7bf
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-04 11:37:01 -0600

    updated

--- src/InheritancePractice.java
+++ src/InheritancePractice.java
@@ -16,13 +16,41 @@ public class InheritancePractice {
 //        }
 //    }
 
+    public void sayHello() {
+        System.out.println("Hello, World!");
+    }
 
+    static class Employee extends InheritancePractice {
+        private double salary;
+        public void doWork() {
+            System.out.println("Work, work...");
+        }
+
+// method overriding
+public void sayHello() {
+    System.out.println("How can I help you?");
+}
+    }
 
 
 
     public static void main(String[] args) {
 //        Employees john = new Employees("John");
 //        john.sayHello();
+    InheritancePractice p = new InheritancePractice();
+    Employee e = new Employee();
+
+    p.sayHello();
+//    e.doWork();
+    e.sayHello();
+
+
+
+
+
+
+
+
 
 }
 
---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,8 @@
+package shapes;
+
+public class Rectangle {
+protected int length;
+protected int width;
+
+
+}
