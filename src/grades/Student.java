f5de5f60c57cd66fc147cbb5efb23285363f1dbd
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-05 14:43:27 -0600

    updated

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 
 public class Student {
 private String name;
-private ArrayList<Integer> grades = new ArrayList<>();
+private ArrayList<Integer> grades;
 
 public Student(String name, Integer grades) {
     this.name = name;
-
+    this.grades = new ArrayList<>();
 }
+
 
 
 
