2645487d817a5601f6b0b1e4b716b79366b34f54
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-05 16:33:00 -0600

    updated

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -28,42 +28,6 @@ public Student(String name) {
     return total / grades.size();
     }
 
-    public static void main(String[] args) {
-    Student matt = new Student("Matt");
-        matt.addGrade(90);
-        matt.addGrade(100);
-        matt.addGrade(70);
-        matt.addGrade(80);
-        matt.addGrade(88);
-    System.out.println(matt.getGradeAverage());
-
-    Student john = new Student("John");
-        john.addGrade(91);
-        john.addGrade(94);
-        john.addGrade(89);
-        john.addGrade(96);
-        john.addGrade(100);
-       System.out.println(john.getGradeAverage());
-
-       Student pierce = new Student("Pierce");
-        pierce.addGrade(91);
-        pierce.addGrade(92);
-        pierce.addGrade(84);
-        pierce.addGrade(90);
-        pierce.addGrade(87);
-        System.out.println(john.getGradeAverage());
-
-        Student fabio = new Student("Fabio");
-        fabio.addGrade(87);
-        fabio.addGrade(88);
-        fabio.addGrade(82);
-        fabio.addGrade(99);
-        fabio.addGrade(80);
-        System.out.println(john.getGradeAverage());
-
-
-
-    }
 
 
 
